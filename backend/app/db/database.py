416df66bc8f7678bc962e68f4a52ebe5aa9da115
RaKa8904 <rakaotaku8904@gmail.com>
2026-01-27 18:47:28 +0530

    just last fixes for render

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://smartpos_db_pcwj_user:WiO4Jm3dHbc3SjIag3tCtCOjAuIsWMns@dpg-d5savt15pdvs739ibpdg-a/smartpos_db_pcwj")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
